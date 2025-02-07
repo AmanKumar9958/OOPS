c489d2301883e5027579b9e1aa7d2194ba35222d
Aman Kumar <amancollege04@gmail.com>
2025-02-07 16:33:27 +0530

    Practice: Access Modifiers

--- accessModifiers.cpp
+++ accessModifiers.cpp
@@ -1,33 +1,28 @@
 #include<iostream>
 using namespace std;
 
-class AccessModifiers{
+class Student{
 public:
     string name;
-    int roll;
+    string course;
 
-    AccessModifiers(){
-        // Default Constructor
+    Student(){
+        // default constructor
+        name = "XYZ";
+        course = "ABC";
+        cgpa = 0;
     }
 
-    AccessModifiers(string name, int roll, float cgpa){
+    Student(string name, string course, float cgpa){
         this->name = name;
-        this->roll = roll;
+        this->course = course;
         this->cgpa = cgpa;
     }
 
     void display(){
         cout<<"Name: "<<name<<endl;
-        cout<<"Roll: "<<roll<<endl;
+        cout<<"Course: "<<course<<endl;
         cout<<"CGPA: "<<cgpa<<endl;
-    }
-
-    float getCgpa(){     // getter
-        return cgpa;
-    }
-
-    void setCgpa(float m){  // setter
-        cgpa = m;
     }
 
 private:
@@ -36,13 +31,8 @@ private:
 
 int main(){
 
-    AccessModifiers stu1("Aman", 6, 7.8);   // we can't change the marks, to still change the marks we have to make a setter method
-    stu1.setCgpa(7.7);
-    stu1.display();
+    Student s1("Aman Kumar", "BCA", 7.8);
+    s1.display();
 
-    AccessModifiers stu2;
-    stu2.name = "Sumit";
-    stu2.roll = 1;
-    // stu2.cgpa = 8;   // private, to still access it we have make a getter method
     return 0;
 }
